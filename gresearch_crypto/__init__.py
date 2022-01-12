663ca0597ffb577c506a45ff0dcb6b3dafa03359
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-12 22:01:42 +0200

    gresearch library

---
+++ gresearch_crypto/__init__.py
@@ -0,0 +1,10 @@
+import sys
+# sys.path.append('/home/ncuong/Programming/g-research-crypto/gresearch_crypto')
+# sys.path.append('/home/ncuong/Programming/g-research-crypto/gresearch_crypto/competition.cpython-37m-x86_64-linux-gnu.so')
+# sys.path.append('/home/ncuong/Programming/g-research-crypto/gresearch_crypto/__init__.py')
+# print(sys.path)
+
+
+from .competition import make_env
+
+__all__ = ['make_env']
