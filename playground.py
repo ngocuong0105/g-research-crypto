de9f644c0756df9d4180ce1b9d18393adb415e26
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-13 10:07:40 +0200

    modularize code

--- playground.py
+++ playground.py
@@ -4,11 +4,13 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 #%%
-train = pd.read_csv("train.csv")
+# Read Data
+data = pd.read_csv("train.csv")
 assets = pd.read_csv("asset_details.csv")
-train.insert(loc=0, column='Time', value=pd.to_datetime(train['timestamp'],unit='s'))
-train.info(show_counts=True)
-
+data.insert(loc=0, column='Time', value=pd.to_datetime(data['timestamp'],unit='s'))
+data.info(show_counts=True)
+train = data[data['timestamp']<1623542400]
+test = data[data['timestamp']>1623542400]
 
 # %%
 # Reproducing the target column
@@ -26,99 +28,11 @@ def ResidualizeMarket(df, mktColumn, window):
 
     return resultRet.drop(columns=[mktColumn]), resultBeta.drop(columns=[mktColumn])
 
-# EVALUATION METRIC
-# 'a' and 'b' are the expected and predicted targets, and ' weights' include the weight of each row, determined by its asset.
-def weighted_correlation(a, b, weights):
-
-  w = np.ravel(weights)
-  a = np.ravel(a)
-  b = np.ravel(b)
-
-  sum_w = np.sum(w)
-  mean_a = np.sum(a * w) / sum_w
-  mean_b = np.sum(b * w) / sum_w
-  var_a = np.sum(w * np.square(a - mean_a)) / sum_w
-  var_b = np.sum(w * np.square(b - mean_b)) / sum_w
-
-  cov = np.sum((a * b * w)) / np.sum(w) - mean_a * mean_b
-  corr = cov / np.sqrt(var_a * var_b)
-
-  return corr
-
 # Function log_return_ahead computes R_t = log(P_{t+16} / P_{t+1})
 # define function to compute log returns
 def log_return(series, periods=1):
     return np.log(series).diff(periods=periods)
 
-
-
-# %%
-ids = list(assets.Asset_ID)
-asset_names = list(assets.Asset_Name)
-
-# times = data['timestamp'].agg(['min', 'max']).to_dict()
-# all_timestamps = np.arange(times['min'], times['max'] + 60, 60)
-
-all_timestamps = np.sort(train['timestamp'].unique())
-targets = pd.DataFrame(index=all_timestamps)
-# %%
-
-price_column = 'Close'
-for i, id in enumerate(ids):
-    asset = train[train.Asset_ID == id].set_index(keys='timestamp')
-    price = pd.Series(index=all_timestamps, data=asset[price_column])
-#   targets[asset_names[i]] = np.log(
-#         price.shift(periods=-16) /
-#         price.shift(periods=-1)
-#     )
-    targets[asset_names[i]] = (
-        price.shift(periods=-16) /
-        price.shift(periods=-1)
-    ) - 1
-
-
-# %%
-# Next calculate M as weighted mean of columns (currently holding R value for each asset) for each row. 
-# This implementation count unavailable R values as zero. 
-
-weights = np.array(list(assets.Weight))
-targets['m'] = np.average(targets.fillna(0), axis=1, weights=weights)
-#%%
-targets = ResidualizeMarket(targets,'m',3750)[0]
-# %%
-diffs = []
-
-for i, id in enumerate(ids):
-    print(asset_names[i])
-    # type: pd.DataFrame
-    asset = train[train.Asset_ID == id].set_index(keys='timestamp')
-    print(f'asset size {asset.shape[0]}')
-    recreated = pd.Series(index=asset.index, data=targets[asset_names[i]])
-    diff = np.abs(asset['Target'] - recreated)
-    diffs.append(diff[~pd.isna(diff)].values)
-    print(f'Average absolute error {diff.mean():8.6f}')
-    print(f'Max absolute error {diff.max():8.6f}')
-    print(f'Standard deviation {diff.std():8.6f}')
-    print(f'Target na {pd.isna(asset.Target).sum()}')
-    print(f'Target_calculated na {pd.isna(recreated).sum()}')
-    print()
-
-diffs = np.concatenate(diffs, axis=0)
-print('For all assets')
-print(f'Average absolute error {diffs.mean():8.6f}')
-print(f'Max absolute error {diffs.max():8.6f}')
-print(f'Standard deviation {diffs.std():8.6f}')
-
-#%%
-prices = train.pivot(index=["timestamp"], columns=["Asset_ID"], values=["Close"])
-prices.columns = [f"A{a}" for a in range(14)]
-
-prices = prices.reindex(range(prices.index[0], prices.index[-1]+60,60), method='pad')
-prices.index = prices.index.map(lambda x: datetime.fromtimestamp(x))
-prices.sort_index(inplace=True)
-
-
-#%%
 # evaluation metric
 def weighted_correlation(actual, pred, weights):
 
@@ -134,5 +48,69 @@ def weighted_correlation(actual, pred, weights):
 
   cov = np.sum((actual * pred * w)) / np.sum(w) - mean_actual * mean_pred
   corr = cov / np.sqrt(var_actual * var_pred)
-
   return corr
+
+# target column computation
+def compute_targets(data:pd.DataFrame, assets:pd.DataFrame, price_col:str):
+    ids = list(assets.Asset_ID)
+    asset_names = list(assets.Asset_Name)
+    # times = data['timestamp'].agg(['min', 'max']).to_dict()
+    # all_timestamps = np.arange(times['min'] times['max'] + 60, 60)
+    all_timestamps = np.sort(data['timestamp'].unique())
+    targets = pd.DataFrame(index=all_timestamps)
+    for i, id in enumerate(ids):
+        asset = data[data.Asset_ID == id].set_index(keys='timestamp')
+        price = pd.Series(index=all_timestamps, data=asset[price_col])
+    #   targets[asset_names[i]] = np.log(
+    #         price.shift(periods=-16) /
+    #         price.shift(periods=-1)
+    #     )
+        targets[asset_names[i]] = (
+            price.shift(periods=-16) /
+            price.shift(periods=-1)
+        ) - 1
+
+    # Next calculate M as weighted mean of columns (currently holding R value for each asset) for each row. 
+    # This implementation count unavailable R values as zero. 
+    weights = np.array(list(assets.Weight))
+    targets['m'] = np.average(targets.fillna(0), axis=1, weights=weights)
+    targets = ResidualizeMarket(targets,'m',3750)[0]
+    return targets
+
+# sanity check
+def compare_targets(data,targets,assets):
+    diffs = []
+    ids = list(assets.Asset_ID)
+    asset_names = list(assets.Asset_Name)
+    for i, id in enumerate(ids):
+        print(asset_names[i])
+        # type: pd.DataFrame
+        asset = data[data.Asset_ID == id].set_index(keys='timestamp')
+        print(f'asset size {asset.shape[0]}')
+        recreated = pd.Series(index=asset.index, data=targets[asset_names[i]])
+        diff = np.abs(asset['Target'] - recreated)
+        diffs.append(diff[~pd.isna(diff)].values)
+        print(f'Average absolute error {diff.mean():8.6f}')
+        print(f'Max absolute error {diff.max():8.6f}')
+        print(f'Standard deviation {diff.std():8.6f}')
+        print(f'Target na {pd.isna(asset.Target).sum()}')
+        print(f'Target_calculated na {pd.isna(recreated).sum()}')
+        print()
+    diffs = np.concatenate(diffs, axis=0)
+    print('For all assets')
+    print(f'Average absolute error {diffs.mean():8.6f}')
+    print(f'Max absolute error {diffs.max():8.6f}')
+    print(f'Standard deviation {diffs.std():8.6f}')
+
+
+#%%
+targets = compute_targets(data,assets,'Close')
+compare_targets(data,targets,assets)
+#%%
+prices = train.pivot(index=["timestamp"], columns=["Asset_ID"], values=["Close"])
+prices.columns = [f"A{a}" for a in range(14)]
+
+prices = prices.reindex(range(prices.index[0], prices.index[-1]+60,60), method='pad')
+prices.index = prices.index.map(lambda x: datetime.fromtimestamp(x))
+prices.sort_index(inplace=True)
+
