fe0ac4f6b31dffbfc1a23d146d22f77f38c6d0e2
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-13 09:44:39 +0200

    clean

--- submission.py
+++ submission.py
@@ -5,10 +5,6 @@ import gresearch_crypto
 env = gresearch_crypto.make_env()
 iter_test = env.iter_test()
 #%%
-i = 0 
 for (test_df, sample_prediction_df) in iter_test:
-    print(i)
     sample_prediction_df['Target'] = 0
     env.predict(sample_prediction_df)
-    i+=1
-
